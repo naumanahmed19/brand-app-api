ee197e2cf8af297e4fe9a92a875b6b2fbc278e5b
Nomi <naumanahmed19@gmail.com>
2020-12-13 00:53:31 +0100

    update home

--- api/BrandHomeController.php
+++ api/BrandHomeController.php
@@ -5,75 +5,29 @@ class BrandHomeController{
     public function get(){
        
             $data = [];
+
+            $sd = [];
             $i = 0;
 
 
             $sections =  get_field('home_screen_sections', 'option');
 
             foreach($sections as $section){
-                $postPerPage = $section['r_number_of_post'];
-
-
-                $data[$i]['title'] = $section['title'];
-                $data[$i]['image'] = $section['image'];
-                $data[$i]['filter'] =$section['filter'];
-                $data[$i]['categories'] =$section['categories'];
-                
-
-                $postType = $section['r_post_type'];
-              
-                
-
-              
-                $args = array(
-                    'posts_per_page'  => $postPerPage,
-                    //'offset'          => $postOffset,
-                    'post_type'       =>  $postType,
-                
-                );
-
-                
-                /** 
-                 * Add Terms
-                 * 
-                */
-                // $terms =  $section['r_category'];
-                // if(!empty($terms)){
-                //     $data[$i]['terms']  = $terms->slug;
-                //     $args['tax_query'] = array(
-                //         array(
-                //             'taxonomy' => $postType.'-categories',
-                //             'field'    => 'slug',
-                //             'terms'    => $terms,
-                //         ),
-                //     );
-                // }
-
-
-
-                // $posts = get_posts($args);
-                
-                // if($section['r_post_type'] == 'album'){
-                
-                //     $data[$i]['albums'] =  $albums->data($posts);
-                // }
-                
-                // if($section['r_post_type'] == 'track'){
-                //     $data[$i]['tracks'] =$tracks->data($posts);
-                // }
-
-                // if($section['r_post_type'] == 'artist'){
-                //     $data[$i]['artists'] =$artists->data($posts);
-                // }
-
-
-                //$data['sections'][$i] = $section['r_post_type'];	
-
+                $sd[$i]['title'] = $section['title'];
+                $sd[$i]['image'] = $section['image'];
+                $sd[$i]['filter'] =$section['filter'];
+                $sd[$i]['categories'] =$section['categories'];
                 $i++;
             }
 
 
-            return $data;
+            $data['categories'] =  get_field('filter_categories', 'option');;
+            $data['sections'] = $sd;
+
+            return [
+             'data' => $data,
+             'status' => 200
+         ];
     }
     
         
