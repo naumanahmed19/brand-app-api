1b18587f43b98024bf757eea41e10e3e8bde2c4a
Nomi <naumanahmed19@gmail.com>
2020-12-30 00:44:02 +0100

    test

--- api/api.php
+++ api/api.php
@@ -200,7 +200,7 @@ function brand_add_custom_data_to_order( $response, $post, $request ) {
     // Get the accessible array of product properties:
     $product = wc_get_product($item['product_id']);
     $productData = $product->get_data();
-    $productData['images'] =woo_get_images($productData);
+    $productData['images'] =woo_get_images($product);
     $products[] = $productData;
     //$products[] = brand_add_custom_data_to_product($product,$product,null);
      
