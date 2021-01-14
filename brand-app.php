6094bb06f611843d91d9e618af140ff2180c11e3
Nomi <naumanahmed19@gmail.com>
2021-01-14 21:36:48 +0100

    update

--- brand-app.php
+++ brand-app.php
@@ -86,6 +86,7 @@ final class Brand_App_API {
 		add_action('wp_enqueue_scripts', 'brand_enqueue_script');
 		
 		function brand_enqueue_script()
+		
 		{   
 			wp_enqueue_style( 'slick', plugin_dir_url( __FILE__ )  . '/assets/css/slick.css' );
 			wp_enqueue_script( 'slick', plugin_dir_url( __FILE__ ) . 'assets/js/slick.min.js', array('jquery'), '1.4.0', false );
