ca95cea4de0cad7d10d279cc50f771c266c6a7c6
Nomi <naumanahmed19@gmail.com>
2021-01-17 19:56:25 +0100

    admin

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,15 +5,7 @@ jQuery(document).ready(function ($) {
 
 
 $(".control-subsection").on('click', function(event){
-    console.log(  document.querySelector('ons-tabbar'));
-    
-    //(... rest of your JS code)
-
-    document.querySelectorAll('iframe').forEach( item =>
-        console.log(item),
-        item.contentWindow.document.body.querySelector('ons-tabbar').setActiveTab(2)
-    );
-});
+    document.querySelector('iframe').contentWindow.document.body.querySelector('ons-tabbar').setActiveTab(1)
 
 });
 
