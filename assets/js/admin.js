aa1674ba404ecc440eca48f1034bfd06b7e251f5
Nomi <naumanahmed19@gmail.com>
2021-01-17 21:42:00 +0100

    admin js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,9 +5,32 @@ jQuery(document).ready(function ($) {
 
 
 $(".control-subsection").on('click', function(event){
-        console.log('click')
-        
-    document.querySelector('iframe').contentWindow.document.body.querySelector('ons-tabbar').setActiveTab(1)
+var id = $(event.target).attr('id');
+    var index = 0;
+  console.log(id);
+  if(id==){
+  
+  }else{
+
+  }
+  switch (id) {
+    case 'accordion-section-sidebar-widgets-brand-home-screen':
+        index = 0;
+        break;
+    case 'accordion-section-sidebar-widgets-brand-search-screen':
+        index = 1;
+        break;
+    case 'accordion-section-sidebar-widgets-brand-favourites-screen':
+        index = 2;
+        break;
+    case 'accordion-section-sidebar-widgets-brand-settings-screen':
+        index = 3;
+        break;
+    default:
+        break;
+  }
+       
+ document.querySelector('iframe').contentWindow.document.body.querySelector('ons-tabbar').setActiveTab(index)
 
 });
 });
