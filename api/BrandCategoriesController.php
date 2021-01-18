6126a0cc7ee4821e41151b03e7cba3dce25bdbda
Nomi <naumanahmed19@gmail.com>
2021-01-18 21:47:49 +0100

    check section

--- api/BrandCategoriesController.php
+++ api/BrandCategoriesController.php
@@ -3,7 +3,7 @@ class BrandCategoriesController{
 
 
     public function get(){
-       
+        $data = [];
         $data['sections'] =  $this->getWidgets();
 
         return  $data;
@@ -18,31 +18,31 @@ public function getWidgets(){
   
     $sections = [];
     $i = 0;
-    foreach( $widgets as $key => $widget){
-      $arr = explode("-",$widget);
+    // foreach( $widgets as $key => $widget){
+    //   $arr = explode("-",$widget);
       
-      $name = $arr[0];
-      $widget_id = $arr[1];
+    //   $name = $arr[0];
+    //   $widget_id = $arr[1];
   
 
-      if($name == 'brandcategorylist_widget'){
-        $sections[$i]['type']='categorylist';
-        $sections[$i]['filter']=null;
-        $items = get_field('category_list', 'widget_' .$widget);
-        //$sections[$i]['title'] = get_field('title', 'widget_' .$widget) ;
-        $ctrl = new BrandHomeController();
+    //   if($name == 'brandcategorylist_widget'){
+    //     $sections[$i]['type']='categorylist';
+    //     $sections[$i]['filter']=null;
+    //     $items = get_field('category_list', 'widget_' .$widget);
+    //     //$sections[$i]['title'] = get_field('title', 'widget_' .$widget) ;
+    //     $ctrl = new BrandHomeController();
    
-        $allSlides = [];
-        foreach ($items as $key => $item ) {
-          $allSlides[$key]['title']  = $item['title'];
-          $allSlides[$key]['categories']  = $ctrl->getCategories($item['categories']);
-        }
+    //     $allSlides = [];
+    //     foreach ($items as $key => $item ) {
+    //       $allSlides[$key]['title']  = $item['title'];
+    //       $allSlides[$key]['categories']  = $ctrl->getCategories($item['categories']);
+    //     }
 
-        $sections[$i]['categories'] =  $allSlides;
-      }
+    //     $sections[$i]['categories'] =  $allSlides;
+    //   }
 
-      $i++;
-      }
+    //   $i++;
+    //   }
   
      return $sections;
   
