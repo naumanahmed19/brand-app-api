773e0f5fbd4393b63efb53472d7798905e5ddf88
Nomi <naumanahmed19@gmail.com>
2021-01-18 22:21:46 +0100

    api update

--- api/BrandCategoriesController.php
+++ api/BrandCategoriesController.php
@@ -4,50 +4,15 @@ class BrandCategoriesController{
 
     public function get(){
         $data = [];
-        $data['sections'] =  $this->getWidgets();
+        $ctrl = new BrandHomeController();
+        $data['sections'] =  $ctrl->getWidgets('brand-search-screen');
 
         return   $data;
     }
 
 
-    
 
-public function getWidgets(){
-    $sidebars_widgets = wp_get_sidebars_widgets();
-    $widgets = $sidebars_widgets['brand-search-screen'];
-  
-    $sections = [];
-   
-    foreach( $widgets as $key => $widget){
-        $arr = explode("-",$widget);
-        
-        $name = $arr[0];
-        $widget_id = $arr[1];
-    
-        if($name == 'brandcategorylist_widget'){
-          $sections['type']= 'categorylist';
-          $sections['categories']  = $this->getCategories($widget);
-        }
-      }
-  
-     return $sections;
-  
-  }
-
-  public function getCategories($widget){
-    $ctrl = new BrandHomeController();
-    $items = get_field('category_list', 'widget_' .$widget);
-   
-    $data = [];
-    foreach ($items as $key => $item ) {
-      $data[$key]['title']  = $item['title'];
-      $data[$key]['categories']  = $ctrl->getCategories($item['categories']);
-    }
-
-    return $data;
-  }
-    
-        
+     
 }
 
 
--- api/BrandHomeController.php
+++ api/BrandHomeController.php
@@ -3,64 +3,9 @@ class BrandHomeController{
 
 
     public function get(){
-       
             $data = [];
-
-            $sd = [];
-            $i = 0;
-
-
-            $sections =  get_field('home_screen_sections', 'option');
-
-            foreach($sections as $section){
-                $sd[$i]['title'] = $section['title'];
-
-             
-                $sd[$i]['type'] =$section['type'];
-              
-                $sd[$i]['filter'] =$section['filter'] ? $section['filter'] : null;
-           
-            
-
-                if($section['type'] == 'product' ){
-                    $ctrl = new BrandProductController();
-                    $args = array(
-                      'posts_per_page'  => $section['posts_per_page'],
-                      //'offset'          => $postOffset,
-                  );
-                  if(!empty($section['category'])) {
-                   $args['category'] =   $section['category']->slug;
-                  }
-              
-                    $sd[$i]['products'] = $ctrl->getPosts($section);
-                }
-
-                if($section['type'] == 'slider' ){
-                  $sd[$i]['slides'] = $this->getSlides($section['slides']);
-                }else{
-
-                if(!empty( $section['image'])){
-                              $sd[$i]['image'] = $section['image'];
-                              }
-
-                //do not add categoires in slider section    
-                    
-                $sd[$i]['categories'] = $this->getCategories($section['categories']);
-
-             
-                }
-
-
-                $i++;
-        
-            }
-
-
             $data['categories'] =  get_field('filter_categories', 'option');
-            $data['sections'] =  $this->getWidgets();
-            // $data['sections'] = $sd;
-
-
+            $data['sections'] =  $this->getWidgets('brand-home-screen');
             return  $data;
     }
 
@@ -86,7 +31,6 @@ class BrandHomeController{
     }
     
     public function getSlides($slides){
-  
       $allSlides = [];
       foreach ($slides as $key => $slide ) {
         $allSlides[$key]['image']  = $slide['image'];
@@ -96,123 +40,20 @@ class BrandHomeController{
       return $allSlides;
     }
 
-
-
+    public function getCategoriesRow($widget){
     
-    // function brand_add_custom_data_to_product( $response, $product) {
-
-    //     $data = $response->get_data();  
-      
-        
-    //     $data['currency'] = get_woocommerce_currency_symbol();
-      
-    //       /**
-    //        * Add Colors
-    //        */
-    //     if(taxonomy_exists( 'pa_color' )){
-      
-    //       $terms = get_terms( 'pa_color' );
-    //       $colors=[];
-    //       foreach($terms as $term){
-    //         $colors[] = get_term_meta( $term->term_id,'color', true );
-    //       }
-        
-    //     }
-      
-    //       /**
-    //        * Add Patterns
-    //        */
-        
-         
-    //     if(taxonomy_exists( 'pa_pattern' )){
-    //       $terms = get_terms( 'pa_pattern' );
-    //       $patterns=[];
-    //       foreach($terms as $term){
-    //         $patterns[] =wp_get_attachment_url( get_term_meta( $term->term_id,'image', true ));
-    //       }
-    //     }
-    
-
-
-    //    $attrs= [];
-    //       foreach($product->get_attributes() as $key => $attr){     
-    //         $attrs[]  = $attr->get_data();
-    //     }
-    //     $data['attributes'] =$attrs;
-
-        
-    //     foreach($data['attributes'] as $key => $attr){
-    //         foreach($data['attributes'][$key]['options'] as $k => $option){
-    //           if($attr['name'] === 'Color'){
-    //               $data['attributes'][$key]['options'][$k] = ['option'=>$option , 'value'=> $colors[$k],'disable'=>false];
-    //           }elseif($attr['name'] === 'Pattern'){
-    //               // $data['attributes'][$key]['name'] = 'Color';
-    //               // $data['attributes'][$key]['type'] = 'pattern';	
-    //               $data['attributes'][$key]['options'][$k] = ['option'=>$option , 'value'=> $patterns[$k],'disable'=>false];	
-    //           }else{
-    //               $data['attributes'][$key]['options'][$k] = ['option'=>$option , 'value'=> $option,'disable'=>false];
-    //           } 
-    //         }
-      
-    //       }
-
-
+      $items = get_field('category_list', 'widget_' .$widget);
      
-    //       if( $product->is_type( 'variable' ) ) {
-    //            $variations = $product->get_available_variations();
-    //              $variations_id = wp_list_pluck( $variations, 'variation_id' );
-    //             $data['variations'] = $variations_id;
-    //       }
- 
-        
-    //    //	$response =  custom_change_product_response($response)
-    //     if(!empty( $data['variations'])){
-    //       $variations = $data['variations'];
-    //       $variations_res = array();
-    //       $variations_array = array();
-    //       if (!empty($variations) && is_array($variations)) {
-    //         foreach ($variations as $variation) {
-    //           $variation_id = $variation;
-    //           $variation = new WC_Product_Variation($variation_id);
-
-              
-
-    //           $variations_res['id'] = $variation_id;
-    //           $variations_res['on_sale'] = $variation->is_on_sale();
-    //           $variations_res['regular_price'] =  (string) $variation->get_regular_price();
-    //           $variations_res['sale_price'] = $variation->get_sale_price();
-    //           $variations_res['sku'] = $variation->get_sku();
-    //           $variations_res['quantity'] = $variation->get_stock_quantity();
-    //           if ($variations_res['quantity'] == null) {
-    //             $variations_res['quantity'] = '';
-    //           }
-    //           $variations_res['stock'] = $variation->get_stock_quantity();
-      
-    //           $attributes = array();
-    //           // variation attributes
-    //           foreach ( $variation->get_variation_attributes() as $attribute_name => $attribute ) {
-    //             // taxonomy-based attributes are prefixed with `pa_`, otherwise simply `attribute_`
-    //             $attributes[] = array(
-    //               'name'   => wc_attribute_label( str_replace( 'attribute_', '', $attribute_name ), $variation ),
-    //               'slug'   => str_replace( 'attribute_', '', wc_attribute_taxonomy_slug( $attribute_name ) ),
-    //               'option' => $attribute,
-    //             );
-      
-    //             $variations_res['variation_attributes'][strtolower( wc_attribute_label( str_replace( 'attribute_', '', $attribute_name ), $variation ))] =  $attribute;
-    //           }
-      
-    //           $variations_res['attributes'] = $attributes;
-    //           $variations_array[] = $variations_res;
-    //         }
-    //       }
-    //       $data['variations'] = $variations_array;
-    //     }
+      $data = [];
+      foreach ($items as $key => $item ) {
+        $data[$key]['title']  = $item['title'];
+        $data[$key]['categories']  = $this->getCategories($item['categories']);
+      }
+  
+      return $data;
+    }
 
 
-
-    //     $data['images'] =woo_get_images($product);
-    //     return  $data;
-    //   }
 
 
 
@@ -224,9 +65,9 @@ class BrandHomeController{
 
 
 
-function getWidgets(){
+function getWidgets($sidebar ){
   $sidebars_widgets = wp_get_sidebars_widgets();
-  $widgets = $sidebars_widgets['brand-home-screen'];
+  $widgets = $sidebars_widgets[$sidebar];
 
   $sections = [];
   $i = 0;
@@ -268,6 +109,10 @@ function getWidgets(){
       $sections[$i]['title'] = get_field('title', 'widget_' .$widget) ;
       $ctrl = new BrandProductController();
       $sections[$i]['products'] =  $ctrl->getProducts($widget);
+    }
+    if($name == 'brandcategorylist_widget'){
+      $sections[$i]['type']= 'categorylist';
+      $sections[$i]['categories']  = $this->getCategoriesRow($widget);
     }
 
 
