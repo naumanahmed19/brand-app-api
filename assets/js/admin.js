a4665e764de70e787d7fb7f4cbab5366ca80a01e
Nomi <naumanahmed19@gmail.com>
2021-02-06 23:33:10 +0100

    check id

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -8,8 +8,10 @@ $(".control-subsection").on('click', function(event){
     var index = 0;
     var id = $(this).attr("id");
 
+    console.log(id);
+
   switch (id) {
-    case 'accordion-section-sidebar-widgets-brand-home-screen':
+    case 'sub-accordion-section-sidebar-widgets-home_screen':
         index = 0;
         break;
     case 'accordion-section-sidebar-widgets-brand-search-screen':
