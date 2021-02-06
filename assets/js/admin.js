3045db6334264bba1f75b84f17f0e6e894b556bf
Nomi <naumanahmed19@gmail.com>
2021-02-07 00:06:35 +0100

    admin js update

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -11,25 +11,32 @@ jQuery(document).ready(function ($) {
         let id = $(this).attr("id");
 
         localStorage.setItem('active_screen', id);
+
+        var str = "accordion-section-sidebar-widgets-brand-home_screen";
+        var screen = str.split("accordion-section-sidebar-widgets-brand-")[1];
+    
         
-    switch (id) {
-        case 'accordion-section-sidebar-widgets-brand-home_screen':
-            index = 0;
-            break;
-        case 'accordion-section-sidebar-widgets-brand-search_screen':
-            index = 1;
-            break;
-        case 'accordion-section-sidebar-widgets-brand-favourites_screen':
-            index = 2;
-            break;
-        case 'accordion-section-sidebar-widgets-brand-settings_screen':
-            index = 3;
-            break;
-        default:
-            break;
-    }
+    // switch (id) {
+    //     case 'accordion-section-sidebar-widgets-brand-home_screen':
+    //         index = 0;
+    //         break;
+    //     case 'accordion-section-sidebar-widgets-brand-search_screen':
+    //         index = 1;
+    //         break;
+    //     case 'accordion-section-sidebar-widgets-brand-favourites_screen':
+    //         index = 2;
+    //         break;
+    //     case 'accordion-section-sidebar-widgets-brand-settings_screen':
+    //         index = 3;
+    //         break;
+    //     default:
+    //         break;
+    // }
         
-    document.querySelector('iframe').contentWindow.document.body.querySelector('ons-tabbar').setActiveTab(index)
+    ///document.querySelector('iframe').contentWindow.document.body.querySelector('ons-tabbar').setActiveTab(index)
+
+    document.querySelector('iframe').contentWindow.document.body.querySelector(screen).style.display = "block";
+
 
     });
 });
