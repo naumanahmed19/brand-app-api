a39af825c3fae760e4c1c8cee0282f36c5061621
Nomi <naumanahmed19@gmail.com>
2021-02-06 23:39:43 +0100

    update screen

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -8,19 +8,17 @@ $(".control-subsection").on('click', function(event){
     var index = 0;
     var id = $(this).attr("id");
 
-    console.log(id);
-
   switch (id) {
-    case 'sub-accordion-section-sidebar-widgets-home_screen':
+    case 'accordion-section-sidebar-widgets-brand-home_screen':
         index = 0;
         break;
-    case 'accordion-section-sidebar-widgets-brand-search-screen':
+    case 'accordion-section-sidebar-widgets-brand-search_screen':
         index = 1;
         break;
-    case 'accordion-section-sidebar-widgets-brand-favourites-screen':
+    case 'accordion-section-sidebar-widgets-brand-favourites_screen':
         index = 2;
         break;
-    case 'accordion-section-sidebar-widgets-brand-settings-screen':
+    case 'accordion-section-sidebar-widgets-brand-settings_screen':
         index = 3;
         break;
     default:
