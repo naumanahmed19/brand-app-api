ee1aaa97af0064fe4b0f44191a62399af9654a78
zabi06 <59314201+Zuhaib50@users.noreply.github.com>
2021-02-08 07:49:11 -0800

    icon padding

--- inc/template-brand.php
+++ inc/template-brand.php
@@ -146,11 +146,13 @@ document.addEventListener('prechange', function(event) {
       <div class="py-5 px-1">
 
         <div class=" max-w-md mx-auto pb-6 h-11 flex m-3 space-x-5">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8    text-gray-500" fill="none" viewBox="0 0 24 24"
+        <div class="pt-4">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8    text-gray-500" fill="none" viewBox="0 0 24 24"
             stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
               d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
           </svg>
+        </div>
 
           <div class="text-gray-500 text-md">
             Sign in | create account  </div>
