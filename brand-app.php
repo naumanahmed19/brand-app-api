f6517064538830383db29aeee76cadda3b07b43e
Nomi <naumanahmed19@gmail.com>
2021-02-19 20:56:20 +0100

    fix wrapper

--- brand-app.php
+++ brand-app.php
@@ -209,7 +209,7 @@ final class Brand_App_API {
 			}
 				
 				$params[0][ 'before_widget' ] .= '<div class="filterDiv '. implode(" ", $ff). '">';
-				$params[0][ 'after_widget' ] == '</div>'
+				$params[0][ 'after_widget' ] == '</div>';
 		
 
 			return $params;
