b9d4ed697d08ca0a5219aee029d8c315e88258b1
Nomi <naumanahmed19@gmail.com>
2021-02-20 01:24:58 +0100

    sss

--- brand-app.php
+++ brand-app.php
@@ -104,9 +104,9 @@ final class Brand_App_API {
 					wp_deregister_style( $wp_style->handle );
 				}
 			}
-		}
-
 		
+
+
 
 			wp_dequeue_style( 'twentytwenty-style');
 		 	wp_enqueue_style( 'material', 'https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css' );
