c8bd8f2f71cfff3c1dde538dbab3c9cf78cdbc61
Nomi <naumanahmed19@gmail.com>
2021-03-17 15:37:21 +0100

    active screen

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,43 +1,62 @@
 jQuery(document).ready(function ($) {
     "use strict";
 
-    
-    // let index =  localStorage.getItem('active_screen');
-    // document.querySelector('iframe').contentWindow.document.body.querySelector('ons-tabbar').setActiveTab(index)
-
 
     $(".control-subsection").on('click', function(event){
-        let index = 0;
-        let str = $(this).attr("id");
+     
+    let str = $(this).attr("id");
+    
+    var screen = str.split("accordion-section-sidebar-widgets-brand-")[1];
 
-        localStorage.setItem('active_screen', str);
-        var screen = str.split("accordion-section-sidebar-widgets-brand-")[1];
+    localStorage.setItem('active_screen', screen);
+
+    let index =  activeScreenIndex(screen);
+        
+    document.querySelector('iframe').contentWindow.document.body.querySelector('ons-tabbar').setActiveTab(index)
+
+    });
+
+
+    function activeScreenIndex(screen){
+
+        let index = 0;
+
+    
 
         console.log(screen);
     
         
-    switch (screen) {
-        case 'home_screen':
-            index = 0;
-            console.log(index);
-            break;
-        case 'search_screen':
-            index = 1;
-            break;
-        case 'favourites_screen':
-            index = 2;
-            break;
-        case 'settings_screen':
-            index = 3;
-            break;
-        default:
-            break;
+        switch (screen) {
+            case 'home_screen':
+                index = 0;
+                console.log(index);
+                break;
+            case 'search_screen':
+                index = 1;
+                break;
+            case 'favourites_screen':
+                index = 2;
+                break;
+            case 'settings_screen':
+                index = 3;
+                break;
+            default:
+                break;
+        }
+        return index;
     }
-        
-    document.querySelector('iframe').contentWindow.document.body.querySelector('ons-tabbar').setActiveTab(index)
 
-    //sdocument.querySelector('iframe').contentWindow.document.body.querySelector(screen).style.display = "block";
+    function activeScreen(){
+        document.querySelector('iframe').contentWindow.document.body.querySelector('ons-tabbar').setActiveTab(index)
+    }
 
 
-    });
+
+    //on init
+
+    
+    let screen = localStorage.getItem('active_screen', str); 
+    activeScreen(activeScreenIndex(screen));
+
+
 });
