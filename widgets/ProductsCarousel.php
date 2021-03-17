db6888cb31b2ff4fab0430a3ed70d863aa3dd21d
Nomi <naumanahmed19@gmail.com>
2021-03-17 14:30:18 +0100

    product carousel fix

--- widgets/ProductsCarousel.php
+++ widgets/ProductsCarousel.php
@@ -14,56 +14,38 @@ class Brand_ProductsCarousel_Widget extends WP_Widget {
 	);
 
 	public function widget( $args, $instance ) {
+
 		echo $args['before_widget'];
-		?>
-
-		<div class="px-5">
-		
-		<?php
-
 		if ( ! empty( $instance['title'] ) ) {
 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
-        }
+		}
 
-        // widget ID with prefix for use in ACF API functions
-		  $widget_id = 'widget_' . $args['widget_id'];
-		  
-		
-		  $ctrl = new BrandProductController();
-		  $products = $ctrl->getProducts($widget_id);
+	// widget ID with prefix for use in ACF API functions
+		$widget_id = 'widget_' . $args['widget_id'];
+		$ctrl = new BrandProductController();
+		$products = $ctrl->getProducts($widget_id);
 		
 		$title = get_field( 'title', $widget_id ) ? get_field( 'title', $widget_id ) : '';
 		$items = intval(get_field('columns', $widget_id));
-		?>
-			<div class="py-5 text-xl font-semibold"><?php  echo esc_attr( $title, 'brand-app' );  ?></div>
-		<?php 
-		
-        echo '<div class="lightSlider" data-pager="false" data-item="'.$items.'">';
-		foreach ($products as $key => $product ) {?>
-			<div>
-				<img src="<?php echo $product['images'][0]['src'] ; ?>" />
-				<div  class="py-2 text-xl">
-				<div><?php echo $product['name'] ; ?></div>
-				<div><?php echo $product['price'] ; ?></div>
-				</div>
-		
-
-		 </div>
-       <?php } ?>
-	   
-        </div>
+	?>
+		<div class="py-5 text-xl font-semibold"><?php  echo esc_attr( $title, 'brand-app' );  ?></div>
+			<div class="lightSlider" data-pager="false" data-item="<?php echo $items; ?>">
+				<?php 
+				foreach ($products as $key => $product ) {?>
+					<div>
+						<img src="<?php echo $product['images'][0]['src'] ; ?>" />
+						<div  class="py-2 text-xl">
+							<div><?php echo $product['name'] ; ?></div>
+							<div><?php echo $product['price'] ; ?></div>
+						</div>
+					</div>
+			<?php } ?>
+			</div>
 		</div>
 
-
-
-		<?php
-
-
+	<?php
 		// Output generated fields
-		
         echo $args['after_widget'];
-        
-        
 	}
 
 	
